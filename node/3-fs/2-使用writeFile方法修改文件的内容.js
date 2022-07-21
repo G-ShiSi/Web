e31e15ae4be793e40f7d1bbcbc3c3ca28f142e78
G-ShiSi <3192523455@qq.com>
2022-07-21 14:32:49 +0800

    日常练习案例

---
+++ node/3-fs/2-使用writeFile方法修改文件的内容.js
@@ -0,0 +1,16 @@
+// 导入fs模块,来操作文件
+const fs = require('fs')
+// 调用fs.readFile方法读取文件
+// 参数1：读取文件的存放路径
+// 参数2：读取文件时采用的编码格式
+// 参数3：回调函数，拿到读取失败和成功的结果
+fs.readFile('./3-fs/test.txt', 'utf8', function (err, result) {
+
+
+    if (err) {
+        console.log('文件读取失败');
+    }
+    else {
+        console.log('读取成功，值为', result);
+    }
+})
